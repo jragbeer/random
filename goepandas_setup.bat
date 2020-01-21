276a7d9044f8d444e16448ca4963a6071314f6e8
JRagbeer <30732548+jragbeer@users.noreply.github.com>
2020-01-21 15:38:55 -0500

    Add files via upload

---
+++ goepandas_setup.bat
@@ -0,0 +1,26 @@
+c:\
+cd C:\Users\J_Ragbeer\PycharmProjects\NYCTaxi\
+start python -m pip install numpy
+pause
+start python -m pip install scikit-learn
+pause
+start python -m pip install GDAL-2.4.1-cp36-cp36m-win_amd64.whl
+pause
+start python -m pip install pyshp
+pause
+start python -m pip install Shapely-1.6.4.post2-cp36-cp36m-win_amd64.whl
+pause
+start python -m pip install Fiona-1.8.6-cp36-cp36m-win_amd64.whl
+pause
+start python -m pip install geopy-1.20.0-py2.py3-none-any.whl
+pause
+start python -m pip install pyproj-2.2.2-cp36-cp36m-win_amd64.whl
+pause
+start python -m pip install geopandas
+pause
+start python -m pip install colorcet
+pause
+start python -m pip install Rtree-0.8.3-cp36-cp36m-win_amd64.whl
+pause
+start python -m pip install pysal
+pause
