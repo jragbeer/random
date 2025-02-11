677446f7e120eb46d3e17722a327c8d0562821dd
JRagbeer <30732548+jragbeer@users.noreply.github.com>
2025-02-11 15:52:48 -0500

    Add files via upload

--- dancing_analysis.py
+++ dancing_analysis.py
@@ -35,6 +35,12 @@ def filter_df(xdf, filters_to_use=None):
                 xdf = xdf[xdf['class_type'].str.contains('salsa')]
             if x == 'bachata':
                 xdf = xdf[xdf['class_type'].str.contains('bachata')]
+            if x == 'level_1':
+                xdf = xdf[xdf['class_level']=="1"]
+            if x == 'level_2':
+                xdf = xdf[xdf['class_level']=="2"]
+            if x == 'level_3':
+                xdf = xdf[xdf['class_level'] == "3"]
     return xdf
 
 def info_string(xdf):
@@ -89,44 +95,79 @@ temp_df = idf[idf['class_level'].str.contains('all')]
 idf.loc[temp_df.index,'class_level'] = "1-2-3-4-5"
 idf['class_level'] = idf['class_level'].str.extract(r'(\d)')
 idf['class_type'] = extract_dance_style(idf['class_type'])
+idf['day_of_week'] = idf['timestamp'].dt.day_name()
+idf['year'] = idf['timestamp'].dt.year
+idf['month'] = idf['timestamp'].dt.month
+
 print(idf.tail(15).to_string())
 
 # ANALYSIS
-not_first_month_df = filter_df(idf, ['not_first_month', 'attended'])
-first_month_df = filter_df(idf, ['first_month', 'attended'])
-first_month_salsa_df = filter_df(idf, ['first_month', 'salsa', 'attended'])
-first_month_bachata_df = filter_df(idf, ['first_month', 'bachata', 'attended'])
+dataframes = {}
+dataframes['attended'] = filter_df(idf, [ 'attended'])
+dataframes['not_first_month'] = filter_df(idf, ['not_first_month', 'attended'])
+dataframes['first_month'] = filter_df(idf, ['first_month', 'attended'])
+dataframes['first_month_salsa'] = filter_df(idf, ['first_month', 'salsa', 'attended'])
+dataframes['first_month_bachata'] = filter_df(idf, ['first_month', 'bachata', 'attended'])
+dataframes['bachata'] = filter_df(idf, ['bachata', 'attended'])
+dataframes['salsa'] = filter_df(idf, ['salsa', 'attended'])
+dataframes['level_1'] = filter_df(idf, ['level_1', 'attended'])
+dataframes['level_2']= filter_df(idf, ['level_2', 'attended'])
+dataframes['level_3'] = filter_df(idf, ['level_3', 'attended'])
+
+for x in idf['teacher'].unique():
+    dataframes[x] = idf[idf['teacher']==x].copy()
+
+per_month_dfs = {}
+per_month_dfs['bachata'] = dataframes['bachata'].groupby(['year', 'month']).agg(bachata=pd.NamedAgg(column="class_type", aggfunc="count"))
+per_month_dfs['salsa'] = dataframes['salsa'].groupby(['year', 'month']).agg(salsa=pd.NamedAgg(column="class_type", aggfunc="count"))
+per_month_dfs['level_1'] = dataframes['level_1'].groupby(['year', 'month']).agg(level_1=pd.NamedAgg(column="class_type", aggfunc="count"))
+per_month_dfs['level_2'] = dataframes['level_2'].groupby(['year', 'month']).agg(level_2=pd.NamedAgg(column="class_type", aggfunc="count"))
+per_month_dfs['level_3'] = dataframes['level_3'].groupby(['year', 'month']).agg(level_3=pd.NamedAgg(column="class_type", aggfunc="count"))
+
+for x in idf['teacher'].unique():
+    per_month_dfs[x] = dataframes[x].groupby(['year', 'month']).agg(teacher=pd.NamedAgg(column="class_type", aggfunc="count"))
+    per_month_dfs[x][x] =per_month_dfs[x]['teacher']
+    per_month_dfs[x] = per_month_dfs[x].drop(columns=['teacher'])
+
+per_month = pd.concat(per_month_dfs.values(), axis=1).fillna(0).astype(int)
+for x in per_month.columns:
+    per_month[x+"_cumsum"] = per_month[x].cumsum()
+
 
 pp = info_string(idf)
 print("\r\rOverall: " + pp)
-print(filter_df(idf, ['attended'])['class_type'].value_counts())
-print(filter_df(idf, ['attended'])['teacher'].value_counts())
-print(filter_df(idf, ['attended'])['class_level'].value_counts())
+print(dataframes['attended']['class_type'].value_counts())
+print(dataframes['attended']['teacher'].value_counts())
+print(dataframes['attended']['class_level'].value_counts())
 print("")
-pp = info_string(not_first_month_df)
+pp = info_string(dataframes['not_first_month'])
 print("\r\rNot First Month: " + pp)
-print(not_first_month_df['class_type'].value_counts())
-print(not_first_month_df['teacher'].value_counts())
-print(not_first_month_df['class_level'].value_counts())
+print(dataframes['not_first_month']['class_type'].value_counts())
+print(dataframes['not_first_month']['teacher'].value_counts())
+print(dataframes['not_first_month']['class_level'].value_counts())
 print("")
 
-pp = info_string(first_month_df)
+pp = info_string(dataframes['first_month'])
 print("\r\rFirst Month: " + pp)
-print(first_month_df['class_type'].value_counts())
-print(first_month_df['teacher'].value_counts())
-print(first_month_df['class_level'].value_counts())
+print(dataframes['first_month']['class_type'].value_counts())
+print(dataframes['first_month']['teacher'].value_counts())
+print(dataframes['first_month']['class_level'].value_counts())
 print("")
-pp = info_string(first_month_salsa_df)
+pp = info_string(dataframes['first_month_salsa'])
 print("\r\rFirst Month + Salsa: " + pp)
-print(first_month_salsa_df['class_type'].value_counts())
-print(first_month_salsa_df['teacher'].value_counts())
-print(first_month_salsa_df['class_level'].value_counts())
+print(dataframes['first_month_salsa']['class_type'].value_counts())
+print(dataframes['first_month_salsa']['teacher'].value_counts())
+print(dataframes['first_month_salsa']['class_level'].value_counts())
 print("")
-pp = info_string(first_month_bachata_df)
+pp = info_string(dataframes['first_month_bachata'])
 print("\r\rFirst Month + Bachata: " + pp)
-print(first_month_bachata_df['class_type'].value_counts())
-print(first_month_bachata_df['teacher'].value_counts())
-print(first_month_bachata_df['class_level'].value_counts())
+print(dataframes['first_month_bachata']['class_type'].value_counts())
+print(dataframes['first_month_bachata']['teacher'].value_counts())
+print(dataframes['first_month_bachata']['class_level'].value_counts())
+
+
+print(per_month.to_string())
+
 
 print("")
 print(datetime.datetime.now()-today)
